6e8c4f6dee91d4a2851220591664c7273e77b58d
= <=>
2016-09-26 10:34:29 +0200

    2nd commit - finshed cmd & file convertion

--- Temperature.rb
+++ Temperature.rb
@@ -3,22 +3,50 @@ attr_accessor :temperature
 attr_accessor :fahrenheit
 attr_accessor :kelvin
 
-def initialize (temperature = 25)
-  puts "what is the temperature?"
-  temperature = gets.to_i
+def initialize (temperature)
+#  puts "what is the temperature?"
+#  temperature = gets.to_i    #voor nadien nog een input te kunnen doorgeven
   @temperature = temperature
 end
 
-def conversion
+def file
+  afile = File.new("data.txt","r")
+  content = afile.sysread(6).to_f
+  @kelvin = content + 273
+  @fahrenheit = content * 1.8 + 32
+  puts "It's currently #{kelvin} degrees kelvin & #{fahrenheit} degrees fahrenheit"
+end
+
+def conversion_cmd
   @kelvin = temperature + 273
   puts "It's currently #{kelvin} degrees kelvin"
   @fahrenheit = temperature * 1.8 + 32
   puts "Its currently #{fahrenheit} degrees fahrenheit"
 end
 
+def give_txt
+ File.write('./temp.txt', 'temperature: ')
+end
+
+def give_json
+
+end
+
+def give_html
+
+end
+
+def read_html
+
+end
+
 end
 
 if __FILE__ == $0
-  temp = TemperatureConverter.new
-  temp.conversion
+  input = ARGV.first.to_f
+  temp = TemperatureConverter.new input
+ temp.conversion_cmd
+#  temp.file
+#  temp.give_txt
+
 end
