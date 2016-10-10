e7fe7fc9d94e32683135cc35af746153ae0c6616
= Nicolas Vandenbroucke <= nicolas.vandenbroucke@hotmail.com>
2016-10-10 11:28:25 +0200

    cleanup

--- app.rb
+++ app.rb
@@ -1,17 +1,20 @@
-require "./converter.rb"
+require "./lib/converter.rb"
 require 'net/http'
 require 'uri'
 
   input = ARGV.first.to_f
    temp = TemperatureConverter.new input
-  temp.conversion_cmd
-  temp.give_json
-  temp.give_html
+  temp.commandline_temperature
+  temp.to_text
+  temp.to_json
+  temp.to_html
 #file version
-  temp.file
-  temp.give_json
-  temp.give_html
+  temp.file_temperature
+  temp.to_text
+  temp.to_json
+  temp.to_html
 #url version
- temp.open ("http://labict.be/software-engineering/temperature/api/temperature/fake")
- temp.give_json
- temp.give_html
+ temp.url_temperature ("http://labict.be/software-engineering/temperature/api/temperature/fake")
+ temp.to_text
+ temp.to_json
+ temp.to_html
--- lib/converter.rb
+++ lib/converter.rb
@@ -2,34 +2,32 @@ class TemperatureConverter
 attr_accessor :temperature
 attr_accessor :fahrenheit
 attr_accessor :kelvin
+KELVIN = 273
 
 def initialize (temperature)
-#  puts "what is the temperature?"
-#  temperature = gets.to_i    #voor nadien nog een input te kunnen doorgeven
   @temperature = temperature
 end
 #read from file and convert
-def file
+def file_temperature
   afile = File.new("data.txt","r")
   @temperature = afile.sysread(6).to_f
-  @kelvin = temperature + 273
+  @kelvin = temperature + KELVIN
   @fahrenheit = temperature * 1.8 + 32
-  puts "It's currently#{temperature}°C, #{kelvin} degrees kelvin & #{fahrenheit} degrees fahrenheit"
+end
+def to_text
+puts "It's currently #{temperature}°C, #{kelvin} degrees kelvin & #{fahrenheit} degrees fahrenheit"
 end
 #standard conversion from command line
-def conversion_cmd
-  @kelvin = temperature + 273
-  puts "It's currently #{kelvin} °K"
+def commandline_temperature
+  @kelvin = temperature + KELVIN
   @fahrenheit = temperature * 1.8 + 32
-  puts "Its currently #{fahrenheit} °F"
-  puts "Its currently #{temperature} °C"
 end
 #convert to json string
-def give_json
+def to_json
  puts "{celsius: #{temperature},fahrenheit: #{fahrenheit}, kelvin: #{kelvin}}"
 end
 #convert to html code
-def give_html
+def to_html
 puts "<div>"
 puts "    <div>#{temperature}°C</div>"
 puts "    <div>#{fahrenheit}°F</div> "
@@ -37,15 +35,10 @@ puts "    <div>#{kelvin}°K</div>"
 puts "</div>"
 end
 #read data from html
-def open(url)
+def url_temperature(url)
 @temperature = Net::HTTP.get(URI.parse(url)).to_f
-@kelvin = temperature + 273
+@kelvin = temperature + KELVIN
 @fahrenheit = temperature * 1.8 + 32
-puts "It's currently #{temperature}°C, #{kelvin}°K & #{fahrenheit}°F"
-end
-
-def method_name
-
 end
 
 end
